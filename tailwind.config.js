a7b552959c0ab3bd90ccc87c60b75d3101e0c41c
Khalteck <khalidoyeneye@gmail.com>
2022-10-23 18:05:30 -0600

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js}",
-    "./public/index.html"
-  ],
+  content: ["./src/**/*.{html,js}", "./public/index.html"],
   theme: {
     extend: {
       fontFamily: {
-        mont: "'Montserrat', sans-serif"
-      }
+        mont: "'Montserrat', sans-serif",
+      },
+      backgroundImage: {
+        home: "url('./images/bg-intro-desktop.png')",
+      },
     },
   },
   plugins: [],
-}
+};
