62570dc5bd4205263ca882baf7acf71ab1ecaf2e
Khalteck <khalidoyeneye@gmail.com>
2022-11-05 18:45:39 -0600

    first page mobile

--- src/Main.js
+++ src/Main.js
@@ -7,6 +7,7 @@ import { useState } from "react";
 import Moviecard from "./Moviecard";
 import Form from "./Form";
 import contact from "./images/matey-man-and-woman-work-in-support-service.png";
+import popcorn from "./images/linus-mimietz-uWjBqbCHY7g-unsplash.jpg";
 
 const Main = () => {
   const [movies] = useState(moviedata);
@@ -21,64 +22,70 @@ const Main = () => {
   return (
     <main className="text-slate-100">
       <section className="w-full min-h-screen bg-[url('./images/lorenzo-bg-image.png')] bg-no-repeat">
-        <div className="w-full min-h-screen bg-[#020d18]/95 p-[50px] flex justify-between items-center">
-          <div className="w-1/2">
+        <div className="w-full min-h-screen bg-[#020d18]/95 px-4 py-[100px] md:p-[50px] block md:flex justify-between items-center">
+          <div className="w-full md:w-1/2">
             <h1 className="text-[1.5rem] text-red-600 font-[700]">
               LORENZO TV
             </h1>
 
-            <p className="text-[3.5rem] font-[700]">
+            <p className="text-[2.3rem] md:text-[3.5rem] font-[700]">
               Unlimited <span className="text-red-600">Movies</span>,<br />
               TV Series, Animes, & lots more.
             </p>
           </div>
-          <div className="w-[40%] relative">
+          <div className="w-full mt-12 md:mt-0 md:w-[40%] relative">
             {/**vector art */}
             <img
               alt="art"
               src={arcs}
-              className="w-[200px] h-[200px] absolute top-[-30px] left-[-30px]"
+              className="w-[80px] h-[80px] md:w-[200px] md:h-[200px] absolute top-[-10px] left-[-10px] md:top-[-30px] md:left-[-30px]"
             />
             {/**vector art */}
-            <img alt="hero" src={hero} className="w-full h-auto rounded-full" />
+            <img
+              alt="hero"
+              src={hero}
+              className="w-[70%] md:w-full h-auto rounded-full"
+            />
           </div>
         </div>
       </section>
 
-      <section className="w-full px-[50px] pt-[80px] pb-[250px] relative">
-        <div className="w-full flex justify-between">
-          <div className="w-[fit-content] relative">
-            <h2 className="text-[2.2rem] font-[700]">Trending Movies</h2>
+      <section className="w-full px-4 md:px-[50px] pt-[80px] pb-[100px] md:pb-[250px] relative">
+        <div className="w-full block md:flex justify-between">
+          <div className="w-full md:w-[fit-content] relative">
+            <h2 className="text-[1.75rem] md:text-[2.2rem] font-[700]">
+              Trending Movies
+            </h2>
             <img
               alt="three-red"
               src={three}
-              className="w-[40px] h-[40px] absolute top-[-20px] right-[-30px]"
+              className="w-[40px] h-[40px] absolute top-[-20px] right-[70px] md:right-[-30px]"
             />
           </div>
-          <div className="text-right">
+          <div className="text-right mt-8 md:mt-0">
             <h3 className="text-[1.5rem] font-[600]">Categories</h3>
-            <div className="mt-[40px] flex gap-[30px]">
-              <button className="px-[20px] py-[5px] rounded-md border-red-700 border-[2px] hover:bg-[#b91c1c] hover:translate-y-[6px] transition-all duration-300">
+            <div className="w-full md:w-[fit-content] mt-4 md:mt-10 flex justify-end gap-4 md:gap-8 text-[0.75rem] md:text-[1rem]">
+              <button className="px-[10px] py-1 h-[fit-content] md:py-2 rounded-md border-red-700 border-[2px] hover:bg-[#b91c1c] hover:translate-y-[6px] transition-all duration-300">
                 Movie
               </button>
-              <button className="px-[20px] py-[5px] rounded-md border-red-700 border-[2px] hover:bg-[#b91c1c] hover:translate-y-[6px] transition-all duration-300">
-                TV Series
+              <button className="px-[10px] py-1 h-[fit-content] md:py-2 rounded-md border-red-700 border-[2px] hover:bg-[#b91c1c] hover:translate-y-[6px] transition-all duration-300">
+                Series
               </button>
-              <button className="px-[20px] py-[5px] rounded-md border-red-700 border-[2px] hover:bg-[#b91c1c] hover:translate-y-[6px] transition-all duration-300">
+              <button className="px-[10px] py-1 h-[fit-content] md:py-2 rounded-md border-red-700 border-[2px] hover:bg-[#b91c1c] hover:translate-y-[6px] transition-all duration-300">
                 Anime
               </button>
-              <button className="px-[20px] py-[5px] rounded-md border-red-700 border-[2px] hover:bg-[#b91c1c] hover:translate-y-[6px] transition-all duration-300">
+              <button className="px-[10px] py-1 h-[fit-content] md:py-2 rounded-md border-red-700 border-[2px] hover:bg-[#b91c1c] hover:translate-y-[6px] transition-all duration-300">
                 Cartoon
               </button>
             </div>
           </div>
         </div>
-        <div className="w-full bg-black/30 mt-[50px] p-[30px] rounded-xl flex gap-[30px] mb-[30px]">
+        <div className="w-full bg-black/30 mt-10 mb-8 p-5 gap-5 rounded-lg grid grid-flow-col place-items-center scroll-auto overflow-y-auto overscroll-x-contain snap-mandatory snap-x no-scrollbar">
           {five?.map((item, index) => {
             return <Moviecard key={index} item={item} />;
           })}
         </div>
-        <button className="px-[20px] py-[10px] rounded-md bg-[#b91c1c] hover:translate-y-[6px] transition-all duration-300">
+        <button className="px-[40px] py-[15px] mt-4 rounded-md bg-[#b91c1c] hover:translate-y-[6px] transition-all duration-300">
           More movies
         </button>
         <svg
@@ -94,18 +101,25 @@ const Main = () => {
         </svg>
       </section>
 
-      <section className="w-full bg-black/30 px-[50px] pb-[80px] text-[2rem]">
-        <div className="w-full flex items-center justify-between">
-          <div className="w-[40%] h-[250px] text-center flex place-items-center border-[3px] border-[#b91c1c]">
-            vector image here
-          </div>
-          <div className="w-1/2 text-right text-[1.4rem]">
+      <section className="w-full bg-black/30 px-4 md:px-[50px] pb-[80px] text-[2rem]">
+        <div className="w-full flex flex-col-reverse md:flex-row items-center justify-between">
+          <img
+            alt=""
+            src={popcorn}
+            className="w-[35%] h-[500px] object-cover rounded-full mx-auto hidden md:block"
+          />
+          <div className="w-full md:w-1/2 text-right text-[1.4rem] mt-8 md:mt-0">
             <div className="w-full text-right">
               <h2 className=" text-[2.2rem] font-[700] mb-5">
                 Download Movies.
                 <br /> Watch Offline.
               </h2>
             </div>
+            <img
+              alt=""
+              src={popcorn}
+              className="w-full my-6 h-[500px] object-cover rounded-full mx-auto md:hidden block"
+            />
             <p>
               Lorem ipsum dolor sit amet, consecetur adipiscing elseddo eiusmod
               There are many variations of passages of lorem Ipsum available,
@@ -119,8 +133,8 @@ const Main = () => {
         </div>
       </section>
       <section className="w-full bg-[#020d18] bg-home text-[2rem]">
-        <div className="overlay w-full h-full bg-[#020d18]/80 px-[50px] py-[80px] flex justify-between text-[1rem]">
-          <div className="w-[40%] text-center">
+        <div className="overlay w-full h-full bg-[#020d18]/80 px-4 md:px-[50px] py-[80px] block md:flex justify-between text-[1rem]">
+          <div className="w-full md:w-[40%] text-center">
             <h2 className="text-[2.5rem] font-bold">Get in Touch</h2>
             <p className="text-[1.4rem]">
               Contact us and we will get back to you soon
@@ -129,12 +143,12 @@ const Main = () => {
               <img
                 alt="three-red"
                 src={three}
-                className="w-[60px] h-[60px] absolute top-[20px] right-[30px]"
+                className="w-8 h-auto md:w-[60px] md:h-[60px] absolute top-[20px] right-[30px]"
               />
               <img alt="" src={contact} className="w-[80%] mx-auto" />
             </div>
           </div>
-          <div className="w-1/2">
+          <div className="w-full md:w-1/2">
             <Form />
           </div>
         </div>
--- src/Moviecard.js
+++ src/Moviecard.js
@@ -1,13 +1,13 @@
 const Moviecard = (props) => {
   return (
-    <div className="bg-[#020d18] w-1/5 h-[470px] p-[15px] rounded-lg text-center relative">
+    <div className="bg-[#020d18] w-[220px] h-[470px] p-4 rounded-lg text-center snap-center snap-always relative">
       <img
         alt=""
         src={props.item.coverImage}
-        className="w-[200px] h-[300px] object-cover mb-[20px]"
+        className="w-full h-[300px] object-cover mb-[20px]"
       />
       <p className="text-[1rem] font-[600] mb-[10px]">{props.item.title}</p>
-      <div className="w-full flex justify-center gap-[10px] text-[0.65rem] tracking-wider absolute bottom-[15px] left-[50%] translate-x-[-50%]">
+      <div className="w-full flex justify-center gap-[10px] text-[0.65rem] tracking-wider absolute bottom-3 left-[50%] translate-x-[-50%]">
         <div className="px-[10px] py-[5px] bg-black/50 rounded-md">PG-18</div>
         <div className="px-[10px] py-[5px] bg-black/50 rounded-md">HD</div>
         <div className="px-[10px] py-[5px] bg-black/50 rounded-md">English</div>
